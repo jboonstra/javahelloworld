f71e00a938554e25d37c0d8ffcfdb03f6ac95982
Joel Boonstra <joel.boonstra@gmail.com>
2015-08-10 12:43:14 -0400

    Fix typo.

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -1,6 +1,6 @@
 public class JavaHelloWorld
 {
-  public stativ void main (String [] args)
+  public static void main (String [] args)
   {
     System.out.println("Java Hello World");
     System.out.println("Hello Again");
